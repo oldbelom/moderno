826fe2834bf9f95d58bb5a578d7d00fb8cd2a00d
oldbelom <oldbelom@gmail.com>
2019-09-18 18:08:36 +0300

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
 $(function(){
 
   var mixer = mixitup('.products__inner-box');
+
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+
+  
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    variableWidth: true
+  });
   
 });
--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ gulp.task('script', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'node_modules/mixitup/dist/mixitup.js'
   ])
   .pipe(concat('libs.min.js'))
@@ -34,6 +35,7 @@ gulp.task('style', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.css',
     'node_modules/magnific-popup/dist/magnific-popup.css',
+    'node_modules/rateyo/src/jquery.rateyo.css',
     'node_modules/normalize.css/normalize.css'
   ])
   .pipe(concat('libs.min.css'))
