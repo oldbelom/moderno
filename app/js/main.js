f12a1c2c792bd4e9c1e5aab383e99a925b9679ad
oldbelom <oldbelom@gmail.com>
2019-09-25 16:17:34 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,11 @@
 $(function(){
 
-  var mixer = mixitup('.products__inner-box');
-
   
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "12px",
     readOnly: true
   });
-
 
   
   $('.product-slider__inner').slick({
@@ -20,4 +17,31 @@ $(function(){
     variableWidth: true
   });
   
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: '$'
+  });
+
+
+  $('.icon-th-list').on('click', function(){
+    $('.product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+
+
+  $('.icon-th-large').on('click', function(){
+    $('.product__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+  });
+
+  
+  var mixer = mixitup('.products__inner-box');
+
+    
 });
--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ gulp.task('script', function(){
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
-    'node_modules/mixitup/dist/mixitup.js'
+    'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
   ])
   .pipe(concat('libs.min.js'))
   .pipe(uglify())
@@ -36,7 +37,8 @@ gulp.task('style', function(){
     'node_modules/slick-carousel/slick/slick.css',
     'node_modules/magnific-popup/dist/magnific-popup.css',
     'node_modules/rateyo/src/jquery.rateyo.css',
-    'node_modules/normalize.css/normalize.css'
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
   ])
   .pipe(concat('libs.min.css'))
   .pipe(cssmin())
