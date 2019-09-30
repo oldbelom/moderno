80996eda390586fbfb5a089358363253581086fc
oldbelom <oldbelom@gmail.com>
2019-09-30 13:39:41 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -41,6 +41,15 @@ $(function(){
   });
 
   
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
+
+  
   var mixer = mixitup('.products__inner-box');
 
     
