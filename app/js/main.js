0fb7bb4ee5c49e02c1e5c6ecec660952d68cbc4c
oldbelom <oldbelom@gmail.com>
2019-10-02 13:12:42 +0300

    added settings page

--- app/js/main.js
+++ app/js/main.js
@@ -51,10 +51,10 @@ $(function(){
   });
 
 
-  $('.product-one__tabs .tab').on('click', function(event) {
+  $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function(event) {
     var id = $(this).attr('data-id');
-      $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-      $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+      $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.product-one__tabs .tabs, .settings__tabs .tab').find('.tab').removeClass('active');
       $(this).addClass('active');
       $('#'+id).addClass('active-tab').fadeIn();
       return false;
